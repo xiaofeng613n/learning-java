0929139b925c8b4f104311757a0234822cf6ca5f
xiaofeng <xiaofeng01@globalegrow.com>
2018-07-06 09:34:02 +0800

    0706

--- learning-log/src/main/java/com/xf/log/LogTest.java
+++ learning-log/src/main/java/com/xf/log/LogTest.java
@@ -2,6 +2,7 @@ package com.xf.log;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 /**
  * Created by xiao on 2018/7/1.
@@ -9,7 +10,20 @@ import org.slf4j.LoggerFactory;
 public class LogTest {
 
 	public static void main(String[] args) {
+		MDC.put("trackId","xx");
 		Logger LOG = LoggerFactory.getLogger(LogTest.class);
 		LOG.info("log:{}",1);
+		MDC.remove("trackId");
+		LOG.info("log:{}",2);
+
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+//				MDC.put("trackId","xx");
+				Logger LOG = LoggerFactory.getLogger(LogTest.class);
+				LOG.info("log:{}",1);
+			}
+		});
+		thread.start();
 	}
 }
